df330910cd8792d2a61f8cfb93b0b975ba6840a0
Yann Duran <yd@live.com.au>
2016-09-06 23:02:06 +1000

    Add Edit Solution command

---
+++ src/Commands/EditSolutionCommand.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.Shell;
+using YD.Framework.VisualStudio.Commands;
+using YD.Framework.VisualStudio.Packages;
+
+namespace Essentials.VS.Commands
+{
+    internal sealed class EditSolutionCommand : DynamicCommand
+    {
+        //***
+
+        private static int CommandId
+            => PackageIds.EditSolutionCommand;
+
+        //===M
+
+        private EditSolutionCommand(PackageBase package) : base(package, CommandId)
+        { }
+
+        //===M
+
+        public static void Instantiate(PackageBase package)
+            => Instantiate(new EditSolutionCommand(package));
+
+        //---
+
+        protected override void OnExecute(OleMenuCommand command)
+            => ExecuteCommand()
+                .ShowProblem()
+                .ShowInformation();
+
+        protected override bool IsActive
+            => base.IsActive
+                && SolutionIsNotBuilding;
+
+        //---
+
+        private CommandResult ExecuteCommand()
+            => Package?.EditSolution();
+
+        //***
+    }
+}
