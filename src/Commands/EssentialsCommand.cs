677b14ecfa27a5fc5dae942b1c51d950d5c9512a
Yann Duran <yd@live.com.au>
2016-10-09 17:24:12 +1100

    Move EssentialsCommand
    - from Commands\Build
    - to Commands
    - fix namespace

---
+++ src/Commands/EssentialsCommand.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Shell;
+
+namespace Essentials.VS.Commands
+{
+    using VS.Options;
+    using YD.Framework.VisualStudio.Commands;
+    using YD.Framework.VisualStudio.Packages;
+
+    internal abstract class EssentialsCommand : DynamicCommand
+    {
+        //***
+
+        protected GeneralDialogPage EssentialsOptions
+            => Package.GetDialogPage(typeof(GeneralDialogPage)) as GeneralDialogPage;
+
+        //===M
+
+        protected EssentialsCommand(PackageBase package, int id) : base(package, id)
+        { }
+
+        //===M
+
+        protected override bool CanExecute
+            => EssentialsOptions.EssentialsEnabled;
+
+        //***
+    }
+}
